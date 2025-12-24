c5c9fde5514a7c37fe5f236b9c9b1174d37bf620
ehgjs2581 <ehgjs2581@gmail.com>
2025-12-24 17:19:14 +0900

    Update report.js

--- api/report.js
+++ api/report.js
@@ -73,7 +73,8 @@ export default async function handler(req, res) {
       }
     });
 
-  } catch (err) {
+ } catch (err) {
     console.error('Report error:', err);
     return res.status(500).json({ ok: false, error: 'Server error' });
+  }
 }
